bef8f2ed90526c879abd67b5ccd1d9c087671b3b
SanyjLiang <910532647@qq.com>
2026-07-11 11:31:03 +0800

    完成2216题--美化数组的最少删除数

---
+++ stack/remove_adjacent/2216.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+class Solution {
+public:
+    int minDeletion(std::vector<int>& nums) {
+        std::stack<int>st;
+        for(auto n:nums)
+        {
+            if(st.size()%2==0)
+            {
+                st.push(n);
+            }else if(st.top()!=n)
+            {
+                st.push(n);
+            }
+        }
+        if(st.size()%2==0)
+        {
+            return nums.size()-st.size();
+        }else {
+            return nums.size()-(st.size()-1);
+        }
+    }
+};
+
+int main(int argc,const char*argv[])
+{
+    std::vector<int>nums;
+    int n=0;
+    std::cin>>n;
+    int val=0;
+    for(int i=0;i<n;i++)
+    {
+        std::cin>>val;
+        nums.push_back(val);
+    }
+    Solution so;
+    int res=so.minDeletion(nums);
+    std::cout<<res<<std::endl<<std::flush;
+    return 0;
+}
